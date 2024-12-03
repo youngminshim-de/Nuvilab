6d3d32ca4bed1beba32b8df1ae9019dc8f662a4d
shimyoungmin <ym.sim@cardoc.kr>
2024-12-04 00:24:10 +0900

    feature: Scenes - 홈 화면 HomeView 구현

---
+++ Nuvilab/Nuvilab/Scenes/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  HomeView.swift
+//  Nuvilab
+//
+//  Created by 심영민 on 12/3/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @StateObject var viewModel: HomeViewModel
+    
+    var body: some View {
+        NavigationStack {
+            contentViewBuilder
+        }
+        .padding(.horizontal, 16)
+        .environmentObject(viewModel)
+        .onAppearOnce { viewModel.onAppearOnce() }
+        .searchable(text: $viewModel.searchText, prompt: "도서명 검색")
+    }
+    
+    @ViewBuilder
+    var contentViewBuilder: some View {
+        switch viewModel.contentState {
+        case .loading:
+            CenterAlignedProgressView()
+        case .empty:
+            EmptySearchView()
+        case .fetchFinished, .fetchMore:
+            SearchResultView()
+        }
+    }
+}
+
+//#Preview {
+//    HomeView()
+//}
+
+
