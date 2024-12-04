b67c6e34b4ce523e6d2625f9d4a760e88864f5fa
shimyoungmin <ym.sim@cardoc.kr>
2024-12-04 17:22:29 +0900

    fix: 이슈 2: 네트워크 연결 불안정 시 앱의 비정상 종료와 관련한 문제해결 방법 추가

--- Nuvilab/Nuvilab/Network/Moya+Extension/Interceptor.swift
+++ Nuvilab/Nuvilab/Network/Moya+Extension/Interceptor.swift
@@ -9,12 +9,18 @@ import Foundation
 import Moya
 import Alamofire
 
+/// 이슈 2: 네트워크 연결 불안정 시 앱의 비정상 종료
+/// 해결
+/// 1. 지수 백오프 알고리즘을 이용하여 재시도 한다.
+
 final class Interceptor: RequestInterceptor {
     private let retryLimit: UInt
     private let exponentialBackoffBase: UInt
     private let exponentialBackoffScale: Double
     private let retryPolicy: RetryPolicy
     
+    /// retryLimit = 2, delayTime: 3sec
+    /// 설정이유 : 무분별한 재시도를 막고 (비용, 리소스 낭비) UX 경험 측면에서 3초이상 아무런 액션이 없으면 이탈률이 높기 때문에
     init(retryLimit: UInt = 2,
          exponentialBackoffBase: UInt = 2,
          exponentialBackoffScale: Double = 1) {
--- Nuvilab/Nuvilab/Scenes/HomeViewModel+APIs.swift
+++ Nuvilab/Nuvilab/Scenes/HomeViewModel+APIs.swift
@@ -26,11 +26,13 @@ extension HomeViewModel {
             bookList = items
             persistenceManager.saveBooks(bookList)
         } catch (let error) {
+            /// 2. 재시도를 하고 난 이후에도 에러가 발생한다면 비정상 종료를 시키는 게 아니라 에러 핸들링을 통해 사용자에게 알린다.
             guard let error = error as? SearchBookError else {
                 apiError = .unknown
                 return
             }
             apiError = error
+            /// 3. Alert를 띄워 Error가 발생했음을 알리고, Core Data Fetch를 통해 오프라인 모드로 동작하게 한다.
             fetchFromCoreData()
         }
     }
