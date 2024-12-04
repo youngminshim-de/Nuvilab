c305cff46fd39addaa9139183ee0029b2dd235ac
shimyoungmin <ym.sim@cardoc.kr>
2024-12-04 14:17:52 +0900

    feature: 앱 실행 초기 셋팅 추가

--- Nuvilab/Nuvilab/ContentView.swift
+++ Nuvilab/Nuvilab/ContentView.swift
@@ -4,18 +4,12 @@
 //
 //  Created by 심영민 on 12/2/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        HomeView(viewModel: HomeViewModel(
+            searchBookAPI: SearchBookAPI(provider: .create())))
     }
 }
 
--- Nuvilab/Nuvilab/NuvilabApp.swift
+++ Nuvilab/Nuvilab/NuvilabApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct NuvilabApp: App {
+    init() {
+        NetworkConnectionManager.shared.startMonitoring()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
